95d258deed713b7aa876bd88bde758b46e2197d9
gurrutiaq <gurrutiaq@gmail.com>
2022-05-12 17:56:46 -0500

    se actualizó la pantalla de inicio, primeras configuraciones de la db

--- app/src/main/java/com/example/finalmax/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/finalmax/AdminSQLiteOpenHelper.java
@@ -6,14 +6,44 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
-    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper
+{
+    private static final String DATABASE_NOMBRE = "max.db";
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super(context, DATABASE_NOMBRE, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase BaseDeDatos) {
-        BaseDeDatos.execSQL("create table articulos(codigo int primary key, " + "descripcion text, precio real)");
+        BaseDeDatos.execSQL("create table alumnos" +            //Se crea la tabla de alumnos
+                "(codigo text primary key, " +
+                "dni text, " +
+                "nombres text, " +
+                "celular text, " +
+                "genero text, " +
+                "edad int, " +
+                "contraseña text)");
+
+        BaseDeDatos.execSQL("create table contactos" +            //Se crea la tabla de alumnos
+                "(codigo int primary key, " +
+                "dni text, " +
+                "nombres text, " +
+                "celular text, " +
+                "genero text, " +
+                "codigo_familiar text, " +
+                "FOREIGN_KEY (text)");
+
+        /*
+        CREATE TABLE track(
+                trackid     INTEGER,
+                trackname   TEXT,
+                trackartist INTEGER,
+                FOREIGN KEY(trackartist) REFERENCES artist(artistid)
+
+         */
+        );
 
     }
 
